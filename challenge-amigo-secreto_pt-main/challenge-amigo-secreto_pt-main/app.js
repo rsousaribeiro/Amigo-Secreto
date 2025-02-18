1faa4a233668fc591088721460b8cb042fcc73ab
Regiane Ribeiro <rsousaribeiro@hotmail.com>
2025-02-18 18:08:19 -0300

    Criando o Projeto Amigo Secreto

--- challenge-amigo-secreto_pt-main/challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/challenge-amigo-secreto_pt-main/app.js
@@ -1,1 +1,47 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+let contaAmigo = 0;
+let li = 0;
+
+function adicionarAmigo() {
+    let nomeAmigo = document.querySelector('input').value;
+    if (nomeAmigo == '') {
+        alert('Por favor, insira um nome.');
+    }
+    else {
+        if (amigos.includes(nomeAmigo)) {
+            alert('Você já incluiu este amigo na lista!'); 
+            limparCampo();
+        }
+        else {
+            amigos.push(nomeAmigo);
+            exibirLista();
+            limparCampo();
+        }    
+    }  
+}
+
+function exibirLista() {
+    document.getElementById('nomeAmigos').innerHTML = '';
+    for (contaAmigo = 0; contaAmigo < amigos.length; contaAmigo++) {
+        let lista = document.getElementById('nomeAmigos').insertRow();
+        lista.innerHTML = amigos[contaAmigo];
+    }
+}
+
+function limparCampo() {
+     document.querySelector('input').value = '';
+     nomeAmigo = '';
+}
+
+function sortearAmigo() {
+    if (amigos.length == 0){
+        alert('Você não incluiu nenhum amigo na lista!'); 
+    }
+    else {
+        let indiceMaximo = amigos.length;
+        let indiceSecreto = parseInt(Math.floor(Math.random() * indiceMaximo));
+        document.getElementById('resultado').innerHTML = amigos[indiceSecreto];
+        limparCampo();
+    }
+}
